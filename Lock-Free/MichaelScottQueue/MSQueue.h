531be5349f92046bc8daab78d15660d96af5e1f0
Chernykh Sergey <s9160605122@gmail.com>
2026-08-23 11:48:41 +0300

    Tune MSQueue memory ordering

--- Lock-Free/MichaelScottQueue/MSQueue.h
+++ Lock-Free/MichaelScottQueue/MSQueue.h
@@ -16,12 +16,15 @@ class MSQueue {
     Node *ownership_head_ {nullptr};
 
 public:
-    MSQueue() : head_(new Node()), tail_(head_.load()), ownership_head_(head_.load()) {}
+    MSQueue() :
+        head_(new Node()),
+        tail_(head_.load(std::memory_order_relaxed)),
+        ownership_head_(head_.load(std::memory_order_relaxed)) {}
     ~MSQueue() {
         Node* head = ownership_head_;
         while (head != nullptr) {
             Node *n = head;
-            head = head->next;
+            head = head->next.load(std::memory_order_relaxed);
             delete n;
         }
     }
@@ -34,33 +37,49 @@ public:
     void push(T value)
     {
         Node *node = new Node{std::move(value), nullptr};
-        Node *t = tail_.load();
+        // мы читаем tail, и сразу его разыминовываем на 66 строке, нода tail должна быть полностью проинициализирована
+        // acquire
+        Node *t = tail_.load(std::memory_order_acquire);
         Node *tn = nullptr;
         do {
             for (;;) {
-                tn = t->next.load();
+                // можем разиминовать tn, после присваивания в t на 54 строке - acquire
+                tn = t->next.load(std::memory_order_acquire);
                 if (tn == nullptr) {
                     break;
                 }
-                if (tail_.compare_exchange_weak(t, tn)) {
+                // успех - обновляем tail_ - release
+                // неудача -  t обновляется, мы его разыменовываем, должны видеть проинициализированную ноду - acquire
+                if (tail_.compare_exchange_weak(t, tn, std::memory_order_release, std::memory_order_acquire)) {
                     t = tn;
                 }
             }
-        } while (!t->next.compare_exchange_weak(tn, node));
-        tail_.compare_exchange_strong(t, node);
+        // после этой точки, все должны увидеть проинициализированную новую ноду
+        // поэтому на успехе - release
+        // при неудаче tn обновляется, но это значение мы игнорируем - relaxed
+        } while (!t->next.compare_exchange_weak(tn, node, std::memory_order_release, std::memory_order_relaxed));
+        // успех - публикация tail - release
+        // неудача - ничего не публиуется, новое значение игнорируется - relaxed
+        tail_.compare_exchange_strong(t, node, std::memory_order_release, std::memory_order_relaxed);
     }
 
     std::optional<T> try_pop()
     {
-        Node *h = head_.load();
+        // мы читаем head, и сразу его разыминовываем на 66 строке, нода head должна быть полностью проинициализирована
+        // acquire
+        Node *h = head_.load(std::memory_order_acquire);
         Node *n = nullptr;
         do {
-            n = h->next;
+            // если мы прочитали не нулевую ноду, то она должны быть проинициализирована
+            // load - acquire
+            n = h->next.load(std::memory_order_acquire);
             if (n == nullptr) {
                 return std::nullopt;
             }
         }
-        while (!head_.compare_exchange_weak(h, n));
+        // успех - обновляем head_ - release
+        // неудача -  h обновляется, мы его разыменовываем, должны видеть проинициализированную ноду - acquire
+        while (!head_.compare_exchange_weak(h, n, std::memory_order_release, std::memory_order_acquire));
         return std::move(n->val);
     }
 };
